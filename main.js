7118cfcb7bcf45b2605f9a9f1a32a6c46244802d
SVSH1 <141404740+SVSH1@users.noreply.github.com>
2023-08-21 16:32:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const body = document.body
+const btn1 = document.getElementById('btn1')
+
+function clickButton() {
+    alert('HAIII SUSHIIII')
+    alert('MAKASII UDAH DI PENCET, di baca yaaa!!')
+    alert('im so very very lucky to have you, when im with you always feel happy, kamu teh selalu berhasil buat aku senyum dan tertawa, kamu juga yang bikin aku tau gimana rasanya di cintai, kamu selalu kasih aku perhatian yang sebelumnya ngga pernah aku dapet dari orang lain, kamu yang selalu setiap dengerin cerita aku meskipun itu cerita random, kamu yang selalu apresiasi aku, dan kamu yang berhasil bikin aku jatuh cinta tampa berusaha, mungkin kamu teh bosen baca kalimat -im so very very lucky to have you- HAHAHAHA tapi aku ngetik ini bukan sekedar ngetik doang, aku bener bener ngerasa beruntung bisa jadi milik kamu')
+    alert('kita udah pernah ngalamin fase kehilangan ya walaupun cuma 1 bulan hehehe, jujur aku antara sedih sama seneng di fase itu, ya seneng nya karena aku merasa bebas aja sii, tapi ngga sama pikiran nya wkwk meski kita udah asing di fase itu tapi kamu masi kepikiran terus sama aku tiap hari, aku juga sering banget liat foto foto kita pas masi deket waktu masa itu. Terusss sedihnya karena aku gaada kamu buat aku ajak lari lagi, ga ada kamu buat aku ajak main kemana mana -walaupun cuma di situ situ doang hehehehe- tapi asik bangett tauu walaupun tempat nya kalo ngga di Miko ya di Binjas HAHAHAHAHA asik yaa karna aku bisa sharing sharing apa ajaa ke kamuu aku bisa dekett sama kamu, teruss aku bisa dengerin cerita cerita random kamu, jujur aku sedih banget sii pas kita di fase kehilangan ituu, padahal waktu itu lagi libur panjang, kita bisa aja ketemu tiap hari kalo ada waktu senggang')
+    alert('aku ga pernah ekspek kita bisa deket lagii sii')
+    alert('masii adaa xixixixi semangat yaa baca nyaa')
+    alert('terakhir da inimaa')
+    alert('aku udah bilang terimakasii banyak banyak ke tuhan karena udah izinin aku buat deket banget sama ciptaannya dia yang inii. aku juga mau bilang kalau aku juga sayang dan cinta banget sama kamu! i really really love you. walaupun kamu kadang ngeselin tapi itu ngga bikin rasa sayang aku ke kamu berkurang, malah bertambah setiap hari nyaa. maaf ya kadang sifat aku masih kekanak-kanakan sampai bikin kamu sering ngalah. terimakasi udah mau sesabar itu ngadepin sifat aku yang kekanak-kanakan, egois, gengsian dan sifat buruk aku yang lainnya. terimakasi juga karena kamu udah mau bertahan sama aku yang suka marah marah, selalu kaya gini ya? tolong jangan pernah berubah. meeting you was one of the best part of my life')
+    alert('im very very lucky have youu')
+    alert('kamu masi jadi orang favorit di hidup aku selama lamanyaa')
+    alert('beressss.. byeee byeeeee makasiii banget udah mau baca sampe akhir. -lurveee')
+    
+}
